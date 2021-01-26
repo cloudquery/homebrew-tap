8732547575cd23b60c5690f1ce9ca758946fe1fe
goreleaserbot <goreleaser@carlosbecker.com>
2021-01-26 18:25:04 +0200

    Brew formula update for cloudquery version v0.8.8

--- cloudquery.rb
+++ cloudquery.rb
@@ -2,16 +2,16 @@
 class Cloudquery < Formula
   desc "Easily monitor and ask questions about your infrastructure."
   homepage "https://cloudquery.io"
-  version "0.8.7"
+  version "0.8.8"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/cloudquery/cloudquery/releases/download/v0.8.7/cloudquery_Darwin_x86_64.zip"
-    sha256 "ec07da1c879328ca9c5b1186be88342a874562f7bc6c304858f24f516ecc4ef1"
+    url "https://github.com/cloudquery/cloudquery/releases/download/v0.8.8/cloudquery_Darwin_x86_64.zip"
+    sha256 "73694142d897802d0c7a3aedcd7a0bb4996a32fa402007bf3a76a96c761f50d0"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/cloudquery/cloudquery/releases/download/v0.8.7/cloudquery_Linux_x86_64.zip"
-      sha256 "aeaf19de17a943edda8e1dfbd1feec162656b6e7b9d09417049a513c0a5bf9c9"
+      url "https://github.com/cloudquery/cloudquery/releases/download/v0.8.8/cloudquery_Linux_x86_64.zip"
+      sha256 "791b8a6e0480281452b9b566791f1fce94e8dd86c874204ae5d6db14934c6fc7"
     end
   end
 
