26eddfcd866443ce743cf7b9a127e7695104dcd7
goreleaserbot <goreleaser@carlosbecker.com>
2021-10-13 10:36:13 +0300

    Brew formula update for cloudquery version v0.15.6

--- cloudquery.rb
+++ cloudquery.rb
@@ -5,28 +5,28 @@
 class Cloudquery < Formula
   desc "Easily monitor and ask questions about your infrastructure."
   homepage "https://cloudquery.io"
-  version "0.15.5"
+  version "0.15.6"
   bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.5/cloudquery_Darwin_arm64.zip"
-      sha256 "6837690f5816a00e07045e3eb12ccce48f66f83c3a494ea121b3a8c70d9d9630"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.5/cloudquery_Darwin_x86_64.zip"
-      sha256 "b250af1476eb636cb5630a6b3e2264b5559cfc516e5795e5b962f28fbd71b15f"
+      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.6/cloudquery_Darwin_x86_64.zip"
+      sha256 "bff4881d6bb4c39921628dbc37ef9be663a4cf36e3875ed101f5b01a97af708c"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.6/cloudquery_Darwin_arm64.zip"
+      sha256 "b3478969f188af83f3c9b726061777fe48d61b9b42746feae1d2b7f185b8d28b"
     end
   end
 
   on_linux do
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.5/cloudquery_Linux_arm64.zip"
-      sha256 "b1e563598363453e339b5f5436a18eef794d584abd9205321eb3d4648b2edf40"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.5/cloudquery_Linux_x86_64.zip"
-      sha256 "3850f0908e931ee09b946abc51d6f460f8d0a454e07e4c7d49bb4c35833393da"
+      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.6/cloudquery_Linux_x86_64.zip"
+      sha256 "e9fe538c75a8df5f0efc2b42d55d560955408117dcb10ca1121589f49fdf0629"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/cloudquery/cloudquery/releases/download/v0.15.6/cloudquery_Linux_arm64.zip"
+      sha256 "9b9cfc8e55f19d8913b641dcc0c10adea080e3baa94bc7862bea11b234795ebe"
     end
   end
 
